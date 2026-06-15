ad45bb5a7083a2f4bd158f9bb790e4877966b8ec
Containerxox <containerxox@gmail.com>
2026-06-15 16:43:07 +0900

    cluster-secret-store.tf 수정

--- modules/addons/cluster-secret-store.tf
+++ modules/addons/cluster-secret-store.tf
@@ -2,20 +2,11 @@
 # ClusterSecretStore
 # -------------------------------------------------------------------
 #
-# ESO가 AWS Secrets Manager에서 시크릿을 읽어올 때 사용하는
-# 클러스터 전체 공유 시크릿 스토어입니다.
+# External Secrets Operator가 AWS Secrets Manager에서 시크릿을 읽어올 때 사용하는
+# 클러스터 전체 공유 SecretStore입니다.
 #
-# 연결 구조:
-#
-# ExternalSecret (nowait-dev namespace)
-#   ↓
-# ClusterSecretStore (cluster-wide)
-#   ↓
-# ESO Pod Identity Association
-#   ↓
-# team6-nowait-dev-eso-role
-#   ↓
-# AWS Secrets Manager
+# 인증은 EKS Pod Identity Association을 통해 ESO Pod에 연결된 IAM Role을 사용합니다.
+# 따라서 IRSA 방식의 auth.jwt.serviceAccountRef는 사용하지 않습니다.
 # -------------------------------------------------------------------
 resource "kubernetes_manifest" "cluster_secret_store" {
   count = var.enable_eso_pod_identity ? 1 : 0
@@ -23,25 +14,22 @@ resource "kubernetes_manifest" "cluster_secret_store" {
   manifest = {
     apiVersion = "external-secrets.io/v1beta1"
     kind       = "ClusterSecretStore"
+
     metadata = {
       name = "team6-nowait-cluster-secret-store"
+
       labels = {
-        team = var.team
+        team        = var.team
+        project     = var.project
+        environment = var.environment
       }
     }
+
     spec = {
       provider = {
         aws = {
           service = "SecretsManager"
           region  = var.region
-          auth = {
-            jwt = {
-              serviceAccountRef = {
-                name      = local.eso_service_account
-                namespace = local.eso_namespace
-              }
-            }
-          }
         }
       }
     }
