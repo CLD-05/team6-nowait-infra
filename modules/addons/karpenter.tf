4c28de680f49a9c06bfee118fc78b79e5269c86d
YunB98 <lyb981112@gmail.com>
2026-06-29 11:12:27 +0900

    feat: monitoring 노드에 Name 태그 추가 (공용 계정 식별용)

--- modules/addons/karpenter.tf
+++ modules/addons/karpenter.tf
@@ -421,6 +421,9 @@ resource "kubernetes_manifest" "karpenter_nodepool_monitoring" {
               effect = "NoSchedule"
             }
           ]
+           tags = {
+            Name = "team6-nowait-prod-monitoring-node"
+          }
           nodeClassRef = {
             group = "karpenter.k8s.aws"
             kind  = "EC2NodeClass"
